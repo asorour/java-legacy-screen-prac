55e8065ea7b73c50c3100a96a2d3398ead35302f
Ahmed Sorour <asorour@gmail.com>
2018-08-21 16:19:36 -0400

    cleanup main function

--- src/main/java/com/neopragma/legacy/screen/JobApplicant.java
+++ src/main/java/com/neopragma/legacy/screen/JobApplicant.java
@@ -6,6 +6,15 @@ import java.util.Scanner;
 
 public class JobApplicant {
 
+	public static final String QUIT_MESSAGE = "quit";
+	public static final String BYE_BYE_MESSAGE = "Bye-bye!";
+	public static final String PROMPT_PLEASE_ENTER_INFO_ABOUT_A_JOB_CANDIDATE_OR_QUIT_TO_QUIT = "Please enter info about a job candidate or 'quit' to quit";
+	public static final String FIRST_NAME_PROMPT = "First name?";
+	public static final String MIDDLE_NAME_PROMPT = "Middle name?";
+	public static final String LAST_NAME_PROMPT = "Last name?";
+	public static final String SSN_PROMPT = "SSN?";
+	public static final String ZIP_CODE_PROMPT = "Zip Code?";
+
 	private Name name;
 	private Ssn ssn;
 	private Address address;
@@ -38,28 +47,32 @@ public class JobApplicant {
 		String zipCode;
 
 		while (!done) {
-			System.out.println("Please enter info about a job candidate or 'quit' to quit");
-			System.out.println("First name?");
-            firstName = scanner.nextLine();		
-            if (firstName.equals("quit")) {
+			System.out.println(PROMPT_PLEASE_ENTER_INFO_ABOUT_A_JOB_CANDIDATE_OR_QUIT_TO_QUIT);
+			
+			firstName = getInput(scanner, FIRST_NAME_PROMPT);
+            if (firstName.equals(QUIT_MESSAGE)) {
             	scanner.close();
-            	System.out.println("Bye-bye!");
-            	done = true;
+            	System.out.println(BYE_BYE_MESSAGE);
             	break;
             }
-			System.out.println("Middle name?");
-            middleName = scanner.nextLine();
-			System.out.println("Last name?");
-            lastName = scanner.nextLine();			
-			System.out.println("SSN?");
-            ssn = scanner.nextLine();			
-			System.out.println("Zip Code?");
-            zipCode = scanner.nextLine();			
-            jobApplicant.setName(firstName, middleName, lastName);          
+            
+            middleName = getInput(scanner, MIDDLE_NAME_PROMPT);
+            lastName = getInput(scanner, LAST_NAME_PROMPT);
+            ssn = getInput(scanner, SSN_PROMPT);
+            zipCode = getInput(scanner, ZIP_CODE_PROMPT);
+
+			jobApplicant.setName(firstName, middleName, lastName);
             jobApplicant.setSsn(ssn);
             jobApplicant.setZipCode(zipCode);
             jobApplicant.save();
 		}
+	}
+
+	private static String getInput(Scanner scanner, String prompt) {
+		String firstName;
+		System.out.println(prompt);
+		firstName = scanner.nextLine();
+		return firstName;
 	}
 
 	private void setName(String firstName, String middleName, String lastName) {
